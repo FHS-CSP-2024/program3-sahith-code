d8335ef2f2b4f0dea1cee6c58b4f7c4e072b08e9
SahithCode <sahith.borige@gmail.com>
2024-09-05 17:48:25 +0000

    Python3Completed

--- Program3.py
+++ Program3.py
@@ -22,12 +22,12 @@ level3 = "semiprofessional"
 lower = 2000
 upper = 3000
 
-print("my name is ", name, " , I am ", age, "years old")
-print("my skills are")
-print("- ", skill1, " (", level1, ")")
-print("- ", skill2, " (", level2, ")")
-print("- ", skill3, " (", level3, " )")
-print("I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
+print("my name is ", name + ", I am ", age, "years old")
+print("\n my skills are")
+print("\t - ", skill1, "("+level1+")")
+print("\t - ", skill2, "("+level2+")")
+print("\t - ", skill3, "("+level3+")")
+print("\n I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
 
 
 
@@ -48,6 +48,18 @@ print("I am looking for a job with a salary of", lower, "-", upper, "dollars per
 #
 # - The program should work correctly even if the values of the variables are changed.
 
-x = input("X val: ")
-y = input("Y val: ")
+x = int(input("x value: "))
+y = int(input("Y val: "))
+
+sum = x+y
+print(str(x), "+", str(y), "=", sum)
+difference = x-y
+print(str(x), "-", str(y), "=", difference)
+product = x*y
+print(str(x), "*", str(y), "=", product)
+quotient = x/y
+print(str(x), "/", str(y), "=", quotient)
+
+
+
 
